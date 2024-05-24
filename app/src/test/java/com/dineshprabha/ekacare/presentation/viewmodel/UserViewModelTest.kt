b82e67c3a0195b4e3aca839339b1efef8914e3e8
Dinesh N <dinesh.prabha1205@gmail.com>
2024-05-24 17:49:38 +0530

    added test cases for room database and hilt

--- app/src/test/java/com/dineshprabha/ekacare/presentation/viewmodel/UserViewModelTest.kt
+++ app/src/test/java/com/dineshprabha/ekacare/presentation/viewmodel/UserViewModelTest.kt
@@ -2,12 +2,18 @@ package com.dineshprabha.ekacare.presentation.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import androidx.room.Room
+import com.dineshprabha.ekacare.data.local.AppDatabase
+import com.dineshprabha.ekacare.data.local.UserDao
 import com.dineshprabha.ekacare.data.model.User
 import com.dineshprabha.ekacare.domain.repository.UserRepository
 import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -18,6 +24,28 @@ import org.mockito.MockitoAnnotations
 
 class UserViewModelTest {
 
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    lateinit var appDatabase: AppDatabase
+    lateinit var userDao: UserDao
+    lateinit var viewModel: UserViewModel
+
+    @Before
+    fun setUp(){
+
+    }
+
+    @Test
+    fun insertUser_expectedSingleUser() = runBlocking{
+
+    }
+
+
+    @After
+    fun tearDown(){
+
+    }
 
 
 
